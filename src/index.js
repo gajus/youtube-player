d661928b14f1cbf70c44bef3a5e9aa85ce1f2722
Ryan Graff <ryantgraff@resistdesign.com>
2017-01-18 05:09:50 -0500

    feat: accept an existing YT Player instance (#38)
    * feat(src/index.js) Add the option to supply an existing YT.Player instance. Close #33.
    
    * fix(src/index.js) Fix doc-comment related linting issues.

--- src/index.js
+++ src/index.js
@@ -17,8 +17,9 @@ let youtubeIframeAPI;
 /**
  * A factory function used to produce an instance of YT.Player and queue function calls and proxy events of the resulting object.
  *
- * @param {HTMLElement|String} elementId Either the DOM element or the id of the HTML element where the API will insert an <iframe>.
- * @param {YouTubePlayer~options} options
+ * @param {HTMLElement|String} elementId Either An existing YT.Player instance,
+ * the DOM element or the id of the HTML element where the API will insert an <iframe>.
+ * @param {YouTubePlayer~options} options See `options` (Ignored when using an existing YT.Player instance).
  * @returns {Object}
  */
 export default (elementId, options = {}) => {
@@ -39,8 +40,18 @@ export default (elementId, options = {}) => {
   options.events = YouTubePlayer.proxyEvents(emitter);
 
   const playerAPIReady = new Promise(async (resolve) => {
-    const YT = await youtubeIframeAPI;
-    const player = new YT.Player(elementId, options);
+    let player;
+
+    if (
+      elementId instanceof Object &&
+      elementId.playVideo instanceof Function
+    ) {
+      player = elementId;
+    } else {
+      const YT = await youtubeIframeAPI;
+
+      player = new YT.Player(elementId, options);
+    }
 
     emitter.on('ready', () => {
       resolve(player);
