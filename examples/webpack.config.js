15f207201915c5262743255288056626a165b3b6
Ryan Graff <ryan@StellarOmega.local>
2017-03-24 00:56:28 -0400

    fix: Fix the babel loader in the example webpack config.

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,41 +1,41 @@
 var path,
-    webpack;
+  webpack;
 
 webpack = require('webpack');
 path = require('path');
 
 module.exports = {
-    devtool: 'source-map',
-    context: path.resolve(__dirname, './src'),
-    entry: {
-        'youtube-player': './youtube-player.js'
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, './src'),
-        colors: true,
-        quiet: false,
-        noInfo: false,
-        publicPath: '/',
-        historyApiFallback: false,
-        host: '127.0.0.1',
-        port: 8000,
-        hot: true
-    },
-    output: {
-        path: __dirname,
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    plugins: [
-        new webpack.NoErrorsPlugin()
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, './src'),
-                loader: 'babel'
-            }
-        ]
-    }
+  devtool: 'source-map',
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    'youtube-player': './youtube-player.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+    colors: true,
+    quiet: false,
+    noInfo: false,
+    publicPath: '/',
+    historyApiFallback: false,
+    host: '127.0.0.1',
+    port: 8000,
+    hot: true
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, './src'),
+        loader: 'babel-loader'
+      }
+    ]
+  }
 };
