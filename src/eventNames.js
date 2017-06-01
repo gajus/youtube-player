b493e5b568b80cb26400b837ed6ded2ce656bb11
hugurp <hugurp@users.noreply.github.com>
2017-06-01 10:00:27 +0200

    feat: add `volumeChange` to `eventNames` (#61)

--- src/eventNames.js
+++ src/eventNames.js
@@ -2,6 +2,8 @@
 
 /**
  * @see https://developers.google.com/youtube/iframe_api_reference#Events
+ * `volumeChange` is not officially supported but seems to work
+ * it emits an object: `{volume: 82.6923076923077, muted: false}`
  */
 export default [
   'ready',
@@ -9,5 +11,6 @@ export default [
   'playbackQualityChange',
   'playbackRateChange',
   'error',
-  'apiChange'
+  'apiChange',
+  'volumeChange'
 ];
